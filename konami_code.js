3c17e54fae7fcc86d2b919d9280350977bbe73ee
hyoyou <heatherhyou@gmail.com>
2017-12-21 00:33:58 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
-
-function init(e) {
+function init() {
+  let index = 0;
+  
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e. detail || e.which);
 
